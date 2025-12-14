d3c1c4fa9b0131e7f1376cf9f7b65fc6d78ece57
Zniouxx <idkomariguess@gmail.com>
2025-12-14 22:10:38 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)  # allows frontend (any port) to connect
+
+@app.route("/send", methods=["POST"])
+def send():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+    print("Received data from frontend:")
+    print("Email:", email)
+    print("Password (demo):", password)
+    return jsonify({
+        "status": "success",
+        "message": "Data received successfully!"
+    })
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+# change this ur port mine is 5000 maybe urs is 5500 or 3000
